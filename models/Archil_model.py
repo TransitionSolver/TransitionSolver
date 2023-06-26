01b78bd9d70d7d5d843bcd4a701c73240f2f161b
Lachlan Morris <lachlan.morris@monash.edu>
2023-06-26 17:52:35 +1000

    Added one-loop Lagrangian parameter extraction to Archil's model.

--- models/Archil_model.py
+++ models/Archil_model.py
@@ -4,10 +4,14 @@ from models.analysable_potential import AnalysablePotential
 
 class SMplusCubic(AnalysablePotential):
     # kap = [-1.87,-1.88,-1.89,-1.9,-1.91,-1.92]*125.**2 /246.
-    def init(self,kap=-121.95,yt=0.9946,g=0.6535,g1=0.35):
+    # Previously, default value for kap was -121.95. Now no default value.
+    # The idea is that if only kap is supplied, the muSq and lam are extracted at the one-loop level. Otherwise, they
+    # are taken as input, presumably from a previous run of the one-loop extraction (e.g. read from saved benchmark).
+    def init(self, kap, muSq=0, lam=0, mu0Sq=0, yt=0.9946, g=0.6535, g1=0.35, bDebugIteration=False):
         self.v = 246.
-        self.v2 = self.v**2
+        self.vSq = self.v**2
         self.mh = 125.
+        self.mhSq = self.mh**2
 
         # The number of scalar fields in the model.
         self.Ndim = 1
@@ -17,9 +21,6 @@ class SMplusCubic(AnalysablePotential):
         self.raddof = 83.25
         # Don't do use simple dof, we'll calculate them via temp and field
         self.bUseSimpleDOF = False
-
-        # setting gs energy via the potential at zero temp
-        self.groundStateEnergy = self.Vtot(self.v, 0.)
 
         # Set the temp and field scale by the EW VEV
         # TODO: these need to be set correctly. vh is copied from the real scalar singlet model but there is vh here.
@@ -32,14 +33,128 @@ class SMplusCubic(AnalysablePotential):
 
         # self.renormScaleSq is the renormalization scale used in the
         # Coleman-Weinberg potential.
-        self.renormScaleSq = self.v**2
+        self.renormScaleSq = self.vSq
 
-        self.kap = kap
-        self.mu2 = .5*(self.mh**2 + self.v*self.kap)
-        self.lam = .5/self.v2 *(self.mh**2 - self.v*self.kap)
         self.yt = yt
         self.g = g
         self.g1 = g1
+
+        self.kap = kap
+        # This is only tree-level extraction. We need one-loop extraction.
+        # self.mu2 = .5*(self.mh**2 + self.v*self.kap)
+        # self.lam = .5/self.v2 *(self.mh**2 - self.v*self.kap)
+
+        self.bValid = True
+
+        if muSq != 0:
+            self.muSq = muSq
+            self.lam = lam
+
+            if mu0Sq != 0:
+                self.mu0Sq = mu0Sq
+
+        if muSq == 0:
+            self.constrainParameters(bDebugIteration)
+        elif mu0Sq == 0:
+            self.calculateQuadraticsForMasses()
+
+        # setting gs energy via the potential at zero temp
+        self.groundStateEnergy = self.Vtot(np.array([self.v]), 0.)
+
+    def calculateQuadraticsForMasses(self):
+        # Tree-level extraction.
+        self.mu0Sq = 0.5*(self.mhSq + self.kap*self.v)
+
+    def HiggsMass(self):
+        return -self.muSq + 2*self.kap*self.v + 3*self.lam*self.vSq + self.d2VCW(np.array([self.v]))
+
+    def constrainParameters(self, bDebugIteration=False):
+        self.muSq, self.lam, self.mu0Sq = 0, 0, 0
+        self.constrainParametersAtTreeLevel()
+        self.muSqPrev, self.lamPrev, self.mu0SqPrev = self.muSq, self.lam, self.mu0Sq
+
+        hasConverged = False
+        i = 0
+        iterMax = 50
+
+        while not hasConverged:
+            if i > iterMax:
+                if bDebugIteration:
+                    print("Failed to converge after", iterMax, "iterations.")
+
+                self.bValid = False
+                return
+
+            if bDebugIteration:
+                print(str(i) + '. ' + str(self.muSq) + '\t' + str(self.lam))
+                print('Higgs mass:', np.sqrt(abs(self.HiggsMass())))
+            i += 1
+
+            self.constrainParametersAtOneLoop()
+
+            if not self.bValid:
+                if bDebugIteration:
+                    print("Invalid at one-loop after iteration:", i)
+                return
+
+            if not self.checkParameterMagnitudes():
+                self.bValid = False
+
+                if bDebugIteration:
+                    print("Invalid at one-loop after iteration:", i)
+
+                # Rollback to the previous parameter values so we can report the unstable set of values before they
+                # diverge.
+                self.rollbackIteration()
+                return
+
+            if bDebugIteration:
+                print(str(abs(self.muSq - self.muSqPrev)) + '\t' + str(abs(self.lam - self.lamPrev)))
+            hasConverged = self.checkConvergence()
+
+            self.muSqPrev, self.lamPrev, self.mu0SqPrev = self.muSq, self.lam, self.mu0Sq
+
+        if bDebugIteration:
+            print("Converged after", i, "iterations")
+            print(str(i) + '. ' + str(self.muSq) + '\t' + str(self.lam) + '\t' + str(self.mu0Sq))
+            print('Higgs mass:', np.sqrt(abs(self.HiggsMass())))
+
+    def constrainParametersAtTreeLevel(self):
+        self.lam = (self.mhSq - self.kap*self.v) / (2*self.vSq)
+        self.muSq = self.kap*self.v + self.lam*self.vSq
+
+        # Store the quadratic parameters calculated from the quadratic parameters using tree-level EWSB. These are used
+        # for masses that enter the Coleman-Weinberg potential.
+        self.mu0Sq = self.muSq
+
+    def constrainParametersAtOneLoop(self):
+        vev = np.array([self.v])
+        dVCW = self.dVCW(vev)
+        d2VCW = self.d2VCW(vev)
+
+        self.lam = (self.mhSq - self.kap*self.v + dVCW/self.v - d2VCW) / (2*self.vSq)
+        self.muSq = self.kap*self.v + self.lam*self.vSq + dVCW/self.v
+
+        # Store the quadratic parameters calculated from the quadratic parameters using tree-level EWSB. These are used
+        # for masses that enter the Coleman-Weinberg potential.
+        self.mu0Sq = self.kap*self.v + self.lam*self.vSq
+
+    def checkParameterMagnitudes(self):
+        quadraticMaxVal = 1e10
+        quarticMaxVal = 100
+
+        if abs(self.muSq) > quadraticMaxVal or abs(self.lam) > quarticMaxVal:
+            # print('Diverging parameters, potential not valid.')
+            return False
+
+        return True
+
+    def rollbackIteration(self):
+        self.muSq, self.lam, self.mu0Sq = self.muSqPrev, self.lamPrev, self.mu0SqPrev
+
+    def checkConvergence(self):
+        tol = 1e-5
+        return abs(self.muSq - self.muSqPrev) < tol and abs(self.lam - self.lamPrev) < tol
 
     def forbidPhaseCrit(self, X):
         return (np.array([X])[...,0] < -5.0).any()
@@ -47,18 +162,111 @@ class SMplusCubic(AnalysablePotential):
     def V0(self, X):
         X = np.asanyarray(X)
         rho = X[...,0]
-        r = -.5*self.mu2*rho**2 + self.kap*rho**3 / 3 + .25*self.lam*rho**4
+        r = -.5*self.muSq*rho**2 + self.kap*rho**3 / 3 + .25*self.lam*rho**4
         return r
 
-    def boson_massSq(self, X, T):
+    def dVCW(self, X, T=0):
+        m, n, c = self.boson_massSq(X, T)
+        dm = self.d_boson_massSq(X)
+
+        y = 0
+
+        for i in range(len(m)):
+            if m[i] > 1e-10:
+                y += n[i]*m[i]*dm[i]*(np.log(np.abs(m[i]/self.renormScaleSq)) - c[i] + 0.5)
+
+        m, n = self.fermion_massSq(X)
+        dm = self.d_fermion_massSq(X)
+        c = 1.5
+
+        for i in range(len(m)):
+            if m[i] > 1e-10:
+                y -= n[i]*m[i]*dm[i]*(np.log(np.abs(m[i]/self.renormScaleSq)) - c + 0.5)
+
+        return y/(32*np.pi**2)
+
+    def d2VCW(self, X, T=0):
+        m, n, c = self.boson_massSq(X, T)
+        dm = self.d_boson_massSq(X)
+        d2m = self.d2_boson_massSq(X)
+
+        y = 0
+
+        for i in range(len(m)):
+            if m[i] > 1e-10:
+                y += n[i]*((dm[i]**2 + m[i]*d2m[i])*(np.log(np.abs(m[i]/self.renormScaleSq)) - c[i] + 0.5)
+                    + dm[i]**2)
+
+        m, n = self.fermion_massSq(X)
+        dm = self.d_fermion_massSq(X)
+        d2m = self.d2_fermion_massSq(X)
+        c = 1.5
+
+        for i in range(len(m)):
+            if m[i] > 1e-10:
+                y -= n[i]*((dm[i]**2 + m[i]*d2m[i])*(np.log(np.abs(m[i]/self.renormScaleSq)) - c + 0.5)
+                    + dm[i]**2)
+
+        return y/(32*np.pi**2)
+
+    def d_boson_massSq(self, X):
         X = np.array(X)
+        rho = X[...,0]
+        g2Sq = self.g**2
+        g1Sq = self.g1**2
+
+        h2 = 6*self.lam*rho + 2*self.kap
+        W2 = g2Sq/2*rho
+        Z2 = (g2Sq + g1Sq)/2*rho
+        ph2 = 0
+
+        M = np.array([h2, W2, Z2, ph2])
+        M = np.rollaxis(M, 0, len(M.shape))
+
+        return M
+
+    def d2_boson_massSq(self, X):
+        g2Sq = self.g**2
+        g1Sq = self.g1**2
+
+        h2 = 6*self.lam
+        W2 = g2Sq/2
+        Z2 = (g2Sq + g1Sq)/2
+        ph2 = 0
+
+        M = np.array([h2, W2, Z2, ph2])
+        M = np.rollaxis(M, 0, len(M.shape))
+
+        return M
+
+    def d_fermion_massSq(self, X):
+        X = np.array(X)
+        rho = X[...,0]
+
+        m12 = self.yt**2 * rho
+
+        massSq = np.array([m12])
+        massSq = np.rollaxis(massSq, 0, len(massSq.shape))
+
+        return massSq
+
+    def d2_fermion_massSq(self, X):
+        m12 = self.yt**2
+
+        massSq = np.array([m12])
+        massSq = np.rollaxis(massSq, 0, len(massSq.shape))
+
+        return massSq
+
+    def boson_massSq(self, X, T):
+        X = np.asanyarray(X)
         rho = X[...,0]
         rhoSq = rho**2
         TSq = T**2
         g2Sq = self.g**2
         g1Sq = self.g1**2
 
-        h2 = 3*self.lam*rhoSq + 2*self.kap*rho - self.mu2 + TSq*(self.lam/4 + g2Sq + (g2Sq + g1Sq)/16 + self.yt**2/4)
+        h2 = 3*self.lam*rhoSq + 2*self.kap*rho - self.mu0Sq + TSq*(self.lam/4 + g2Sq + (g2Sq + g1Sq)/16 + self.yt**2/4)
         W2 = g2Sq/4*rhoSq + 11/6*g2Sq*TSq
         a = (g2Sq + g1Sq)/4*rho**2 + 11/6*(g2Sq + g1Sq)*TSq
         Delta = np.sqrt(a**2 - 11/3*g1Sq*g2Sq*(11/3 + rhoSq)*TSq)
@@ -69,7 +277,7 @@ class SMplusCubic(AnalysablePotential):
         M = np.rollaxis(M, 0, len(M.shape))
 
         dof = np.array([1, 6, 3, 3])
-        c = np.array([1.5, 5/6, 5/6])
+        c = np.array([1.5, 5/6, 5/6, 5/6])
 
         return M, dof, c
 
@@ -89,3 +297,8 @@ class SMplusCubic(AnalysablePotential):
     def approxZeroTMin(self):
         # There are generically two minima at zero temperature in this model, and we want to include both of them.
         return [np.array([0]), np.array([self.v])]
+
+
+if __name__ == "__main__":
+    potential = SMplusCubic(-1.9*125**2/246, bDebugIteration=True)
+
