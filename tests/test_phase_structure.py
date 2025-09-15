ab7355ceca9316ef1c3875a7fa5735c07ea11751
andrewfowlie <andrew.j.fowlie@gmail.com>
2025-09-15 10:53:29 +0800

    fix test error on github

--- tests/test_phase_structure.py
+++ tests/test_phase_structure.py
@@ -22,6 +22,10 @@ def test_phase_structure():
     result = phasetracer.phase_structure(
         potential, phase_structure_file, vw=0.9)
 
+    # sanitise result to avoid float/np.float64 differences
+    # with disk
+    result = json.loads(json.dumps(result))
+
     with open(THIS / "rss_bp1_phase_structure.json", "r") as f:
         expected = json.load(f)
 
