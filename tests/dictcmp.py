35c88980d7caa584588d010478505489b959efe0
andrewfowlie <andrew.j.fowlie@gmail.com>
2025-09-15 15:14:44 +0800

    allow list of dicts

--- tests/dictcmp.py
+++ tests/dictcmp.py
@@ -32,11 +32,34 @@ def isclose(result, file_name, rtol=1e-3, atol=0., ignore=None):
         except TypeError:
             assert a == b
 
-    for k in set(expected) - set(ignore):
+    for i in ignore:
+        del expected[i]
+
+    return _isclose(result, expected, assert_approx_equal)
+
+
+def is_list_dict(l):
+    """
+    @returns Whether list is a list of dictionaries
+    """
+    return isinstance(l, list) and all([isinstance(e, dict) for e in l])
+
+
+def _isclose(result, expected, assert_equal):
+    """
+    Walk through data and perform check
+    """
+    if not isinstance(expected, dict):
+        return assert_equal(result, expected)
+
+    for k in expected:
         if isinstance(expected[k], dict):
             for l in expected[k]:
-                assert_approx_equal(result[k][l], expected[k][l])
+                _isclose(result[k][l], expected[k][l], assert_equal)
+        elif is_list_dict(expected[k]):
+            for a, b in zip(result[k], expected[k]):
+                _isclose(a, b, assert_equal)
         else:
-            assert_approx_equal(result[k], expected[k])
+            _isclose(result[k], expected[k], assert_equal)
 
     return True
