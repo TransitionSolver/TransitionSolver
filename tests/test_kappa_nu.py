c950ccc8b6743b549f8efed6faeaf41414552862
andrewfowlie <andrew.j.fowlie@gmail.com>
2025-09-24 09:33:06 +0800

    add test of kappa nu model

---
+++ tests/test_kappa_nu.py
@@ -0,0 +1,15 @@
+"""
+Test kappa nu model
+===================
+"""
+
+import numpy as np
+
+from TransitionSolver.gws import kappa_nu_model
+
+
+def test_kappa_nu_model():
+    cs2 = 0.317458236000095
+    al = 1.0219991793900685
+    vp = 0.9288690647849615
+    assert np.isclose(kappa_nu_model(cs2, al, vp), 0.6128251350166324)
