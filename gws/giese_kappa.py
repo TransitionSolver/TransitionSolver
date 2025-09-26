aa9fdcccef94fb09d2e14ea0120666fcb824b8dd
andrewfowlie <andrew.j.fowlie@gmail.com>
2025-09-26 09:57:34 +0800

    now understand giese model; make code better reflect equations/model

--- gws/giese_kappa.py
+++ gws/giese_kappa.py
@@ -1,10 +1,26 @@
 """
 Efficiency formula
 ==================
+
+Based on https://arxiv.org/abs/2004.06995
 """
 
 import numpy as np
 from scipy.integrate import solve_ivp, quad
+
+
+def mu(xi, v):
+    """
+    @returns Eq. 16 for boosted fluid velocity
+    """
+    return (xi - v) / (1. - xi * v)
+
+
+def gamma(v):
+    """
+    @returns Lorentz factor below eq. 5
+    """
+    return (1. - v**2)**-0.5
 
 
 def kappa_nu_model(cs2, al, vp, use_cj=True):
@@ -33,41 +49,54 @@ def kappa_nu_model(cs2, al, vp, use_cj=True):
     else:
         # analytic result in if using Chapman-Jouget velocity
         vm = np.sqrt(cs2)
-        
-    def dfdv(v, xiw):
-        xi, w = xiw
-        # below eq. 5 - Lorentz factor
-        gamma_2 = 1. / (1. - v**2)
-        # eq. 16 for boosted fluid velocity
-        mu = (xi - v) / (1. - xi * v)
-        # eq. 14
-        dxidv = 0.5 * gamma_2 * (1. - v * xi) * (mu**2 / cs2 - 1.) * xi / v
-        # eq. 15
-        dwdv = nu * gamma_2 * mu * w 
-        return [dxidv, dwdv]
 
-    y0 = [vp, 1.]  # boundary condition
-    t_span = [(vp - vm) / (1. - vp * vm), 0]
-    sol = solve_ivp(dfdv, t_span, y0, vectorized=True, dense_output=True)
-    
-    def integrand(v):
-        xi, w = sol.sol(v)
-        dxidv = dfdv(v, [xi, w])[0]
-        # below eq. 5 - Lorentz factor
-        gamma_2 = 1. / (1. - v**2)
-        # eq. 5 integrand, though integrating wrt v hence dxidv
-        return dxidv * xi**2 * v**2 * gamma_2 * w
-        
-    # eq. 5
-    rho_fl = 3. / vp**3 * quad(integrand, t_span[1], t_span[0])[0]
-    
-    # eq. 54 for enthalpy ratio
+    def dxi_dv(v, xi):
+        r"""
+        @returns Eq. 14
+        """
+        return 0.5 * gamma(v)**2 * (1. - v * xi) * (mu(xi, v)**2 / cs2 - 1.) * xi / v
+
+    def dw_dv(v, xi, w):
+        r"""
+        @returns Eq. 15
+        """
+        return nu * gamma(v)**2 * mu(xi, v) * w
+
+    def dxi_w_dv(v, xi_w):
+        r"""
+        @returns Eq. 14 and 15
+        """
+        xi, w= xi_w
+        return [dxi_dv(v, xi), dw_dv(v, xi, w)]
+
+    # eq. 54 for enthalpy
     wm = (vp / vm) * (1. / cs2 + 3. * al) - 1. + 3. * al 
     wp = 1. / cs2 - vp / vm
-    r = wp / wm
-    
+
+    # boundary condition for \xi
+    xi0 = vp
+    # boundary condition for w
+    w0 = wm
+    # boundaries for v
+    v0 = mu(vp, vm)
+    v1 = 0.
+    # solve system of differential equations for \xi and w
+    sol = solve_ivp(dxi_w_dv, [v0, v1], [xi0, w0], vectorized=True, dense_output=True)
+
+    def rho_fl_integrand(v):
+        """
+        Note that integrating wrt v hence dxidv factor
+        
+        @returns Eq. 5 integrand
+        """
+        xi, w = sol.sol(v)
+        return dxi_dv(v, xi) * xi**2 * v**2 * gamma(v)**2 * w
+
+    # eq. 5
+    rho_fl = 3. / vp**3 * quad(rho_fl_integrand, v1, v0)[0]
+
     # eq. 34
-    D_theta = 3. * al * r  # TODO this doesn't match the eq. 34
-    
+    D_theta = 3. * al * wp
+
     # eq. 36 for kappa
     return 4 * rho_fl / D_theta
