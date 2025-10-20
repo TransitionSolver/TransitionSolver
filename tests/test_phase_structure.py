413b5c0b3798bc304327cbbb9a28de2a5532b900
andrewfowlie <andrew.j.fowlie@gmail.com>
2025-10-20 11:12:37 +0800

    switch test to c++ potential

--- tests/test_phase_structure.py
+++ tests/test_phase_structure.py
@@ -9,19 +9,16 @@ from pathlib import Path
 
 import numpy as np
 
-from TransitionSolver import phasetracer
-from TransitionSolver.models.real_scalar_singlet_model_boltz import RealScalarSingletModel_Boltz as RealScalarSingletModel
+from TransitionSolver import phasetracer, RSS_BP1
 from dictcmp import isclose
 
 THIS = Path(os.path.dirname(os.path.abspath(__file__)))
 
 
 def test_phase_structure():
-    point = THIS / 'rss_bp1.txt'
     phase_structure_file = THIS / "rss_bp1_phase_structure.dat"
-    potential = RealScalarSingletModel(*np.loadtxt(point))
     result = phasetracer.phase_structure(
-        potential, phase_structure_file, vw=0.9)
+        RSS_BP1, phase_structure_file, vw=0.9)
     # TODO ignoring T and SonT as the size of them changes between local & github
     # but good to reinclude them later
     assert isclose(result, THIS / "rss_bp1_phase_structure.json", ignore=['analysisTime', 'T', 'SonT'], rtol=5e-2, atol=1e-9)
