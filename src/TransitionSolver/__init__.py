c35f32f3feb3267f56c737df2822262a5f48ee3e
andrewfowlie <andrew.j.fowlie@gmail.com>
2025-12-17 14:00:39 +0800

    rm methods that only used for cppyy

--- src/TransitionSolver/__init__.py
+++ src/TransitionSolver/__init__.py
@@ -5,3 +5,4 @@ TransitionSolver
 
 from .effective_potential import load_potential
 from .benchmarks import RSS_BP1
+from .phasetracer_subprocess import build_phase_tracer, run_phase_tracer, phase_history
