4e3a5dbf9357f7a1a32e3091eca1be3d0864bd6e
Peter Athron <peter.athron@coepp.org.au>
2026-01-01 17:23:38 +0800

    For now chnage settings to match RSS.cpp in PT

--- src/TransitionSolver/interface.cpp
+++ src/TransitionSolver/interface.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 #include <stdexcept>
 #include <vector>
-
+#include <eigen3/Eigen/Eigenvalues>
 #include "phasetracer.hpp"
 
 
@@ -33,13 +33,28 @@ int main(int argc, char *argv[]) {
     std::vector<double> point = read_point(argv[1]);
     MODEL_NAME_WITH_NAMEPSPACE model(point);
     model.set_daisy_method(EffectivePotential::DaisyMethod::Parwani);
-    model.set_bUseBoltzmannSuppression(false);
+    model.set_bUseBoltzmannSuppression(true);
     model.set_xi(0);
     model.set_useGSResummation(true);
+    Eigen::VectorXd origin(2);
+    origin << 0.0, 0.0;
+    Eigen::VectorXd vev = model.get_EW_VEV();
     
     PhaseTracer::PhaseFinder pf(model);
+
+    double maxT = -1;
+    double maxTemp = maxT;
+
+    if (maxT < 0) {
+      double temperatureScale = model.get_temperature_scale();
+      // Start just above the maximum temperature at which there are two phases so we don't have an erroneous high
+      // temperature subcritical transition.
+      maxTemp = model.getMaxTemp(temperatureScale, temperatureScale * 10, 500, temperatureScale * 0.01,temperatureScale * 0.001) * 1.01;
     
-    pf.set_t_high(253.20465304119307);
+    }
+    //pf.set_t_high(253.20465304119307);
+    pf.set_t_high(maxTemp);
+  
     pf.set_check_vacuum_at_high(false);
     pf.set_seed(0);
     pf.set_check_hessian_singular(false);
