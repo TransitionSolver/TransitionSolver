32334d4688d549d5336e1cb83c0c7c62f2a21f18
andrewfowlie <andrew.j.fowlie@gmail.com>
2026-01-02 10:50:27 +0800

    update gw baseline in tests now that vw handled consistently

--- tests/test_gws.py
+++ tests/test_gws.py
@@ -39,7 +39,7 @@ def test_plot_gw():
 
 def test_snr():
     snr = lisa.SNR(analyser.gw_total)
-    assert np.isclose(snr, 303.08379898108984)
+    assert np.isclose(snr, 61.51393706755697)
     
     
 @pytest.mark.mpl_image_compare
