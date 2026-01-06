1b2700ffeea0902959c4e54c4c50493e013e8b18
andrewfowlie <andrew.j.fowlie@gmail.com>
2026-01-06 12:31:52 +0800

    tweak plot settings

--- src/TransitionSolver/plot.py
+++ src/TransitionSolver/plot.py
@@ -3,8 +3,9 @@ Make plots from results on disk
 ===============================
 """
 
-import json
+import json_numpy as json
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 def load_transition(transition_id, phase_structure):
@@ -21,24 +22,31 @@ def load_transition(transition_id, phase_structure):
     raise RuntimeError(f"Could not find {transition_id} transition")
 
 
-def add_temp_vline(ax, x, label, color):
+def add_labeled_vline(ax, x, label, color):
     transform = ax.get_xaxis_transform()
     y = 0.8
     ax.axvline(x, c=color, ls=':')
-    ax.text(x, y, label,  c=color, transform=transform, ha='left')
+    ax.text(x, y, label,  c=color, transform=transform, ha='center', bbox={'fc': 'w', 'ec': color})
 
 
-def add_temp_vlines(transition, ax):
+def add_labeled_hline(ax, y, label, color):
+    transform = ax.get_yaxis_transform()
+    x = 0.1
+    ax.axhline(y, c=color, ls=':')
+    ax.text(x, y, label,  c=color, transform=transform, va='center', bbox={'fc': 'w', 'ec': color})
+
+
+def add_labeled_vlines(transition, ax):
     if 'TGammaMax' in transition:
-        add_temp_vline(ax, transition['TGammaMax'], "$T_\Gamma$", 'k')
+        add_labeled_vline(ax, transition['TGammaMax'], "$T_\Gamma$", 'k')
     if 'Tn' in transition:
-        add_temp_vline(ax, transition['Tn'], "$T_n$", 'r')
+        add_labeled_vline(ax, transition['Tn'], "$T_n$", 'r')
     if 'Tp' in transition:
-        add_temp_vline(ax, transition['Tp'], "$T_p$", 'g')
+        add_labeled_vline(ax, transition['Tp'], "$T_p$", 'g')
     if 'Te' in transition:
-        add_temp_vline(ax, transition['Te'], "$T_e$", 'b')
+        add_labeled_vline(ax, transition['Te'], "$T_e$", 'b')
     if 'Tf' in transition:
-        add_temp_vline(ax, transition['Tf'], "$T_f$", 'gold')
+        add_labeled_vline(ax, transition['Tf'], "$T_f$", 'm')
 
 
 def plot_volume(transition_id, phase_structure=None, phase_structure_file=None, ax=None, show=False):
@@ -65,10 +73,9 @@ def plot_volume(transition_id, phase_structure=None, phase_structure_file=None, 
         ax.axvspan(transition['TVphysDecr_low'],
                    transition['TVphysDecr_high'], alpha=0.3, color='r', zorder=-1)
                    
-    add_temp_vlines(transition, ax)
-
-    ax.axhline(3., c='gray', ls=':', zorder=-1)
-    ax.axhline(0., c='gray', ls=':', zorder=-1)
+    add_labeled_vlines(transition, ax)
+    add_labeled_hline(ax, 3., "3", 'gray')
+    add_labeled_hline(ax, 0., "0", 'gray')
 
     ax.set_xlabel('$T$ (GeV)')
     ax.set_ylabel(
@@ -99,7 +106,7 @@ def plot_vw(transition_id, phase_structure=None, phase_structure_file=None, ax=N
 
     ax.plot(transition['TSubSampleArray'], transition['vw_samples'])
  
-    add_temp_vlines(transition, ax)
+    add_labeled_vlines(transition, ax)
  
     ax.set_xlabel('$T$ (GeV)')
     ax.set_ylabel('Bubble wall velocity, $v_w$')
@@ -135,7 +142,7 @@ def plot_gamma(transition_id, phase_structure=None, phase_structure_file=None, a
     ax.set_xlabel('$T$ (GeV)')
     ax.set_ylabel('Transition rate, $\\Gamma(T)$')
 
-    add_temp_vlines(transition, ax)
+    add_labeled_vlines(transition, ax)
 
     ax.legend()
 
@@ -169,7 +176,7 @@ def plot_bubble_radius(transition_id, phase_structure=None, phase_structure_file
     ax.set_xlabel('$T$ (GeV)')
     ax.set_ylabel('Bubble radius, $\\overline{R}_B(T)$')
     
-    add_temp_vlines(transition, ax)
+    add_labeled_vlines(transition, ax)
 
     if show:
         plt.show()
@@ -201,7 +208,7 @@ def plot_bubble_separation(transition_id, phase_structure=None, phase_structure_
     ax.set_xlabel('$T$ (GeV)')
     ax.set_ylabel('Bubble separation, $R_*(T)$')
     
-    add_temp_vlines(transition, ax)
+    add_labeled_vlines(transition, ax)
 
     if show:
         plt.show()
@@ -233,7 +240,7 @@ def plot_bubble_number(transition_id, phase_structure=None, phase_structure_file
     ax.plot(transition['TSubSampleArray'],
             transition['totalNumBubbles'], ls="--", label='$N^{\\mathrm{ext}}(T)$')
 
-    add_temp_vlines(transition, ax)
+    add_labeled_vlines(transition, ax)
 
     ax.set_yscale('log')
     ax.legend()
@@ -265,7 +272,7 @@ def plot_pf(transition_id, phase_structure=None, phase_structure_file=None, ax=N
 
     ax.plot(transition['TSubSampleArray'], transition['Pf'])
 
-    add_temp_vlines(transition, ax)
+    add_labeled_vlines(transition, ax)
     ax.set_yscale('log')
     ax.set_xlabel('$T$ (GeV)')
     ax.set_ylabel('False vacuum fraction, $P_f(T)$')
@@ -295,7 +302,7 @@ def plot_action_curve(transition_id, phase_structure=None, phase_structure_file=
 
     ax.plot(transition['T'], transition['SonT'])
 
-    add_temp_vlines(transition, ax)
+    add_labeled_vlines(transition, ax)
 
     ax.set_xlabel('$T$ (GeV)')
     ax.set_ylabel('$S(T) / T$')
@@ -320,14 +327,19 @@ def plot_summary(phase_structure=None, phase_structure_file=None, show=False):
 
     transitions = phase_structure['transitions']
     plotters = [plot_volume, plot_vw, plot_gamma, plot_bubble_radius, plot_bubble_separation, plot_bubble_number, plot_action_curve, plot_pf]
-    fig, ax = plt.subplots(len(plotters), len(transitions))
+    fig, ax = plt.subplots(len(plotters), len(transitions), constrained_layout=False, sharex='col', figsize=(10 * len(transitions), 6 * len(plotters)))
+    ax_arr = np.atleast_2d(ax)
 
-    for y, tr in enumerate(transitions):
-        for x, p in enumerate(plotters):
-            p(tr['id'], phase_structure, ax=ax[x, y])
+    for x, tr in enumerate(transitions):
+        for y, p in enumerate(plotters):
+            this_ax = ax_arr[x, y]
+            p(tr['id'], phase_structure, ax=this_ax)
+            this_ax.set_xlabel(None)
+
+    fig.supxlabel("$T$ (GeV)", y=0.005)
+    fig.set_tight_layout(True)
 
     if show:
-        plt.tight_layout()
         plt.show()
 
     return fig
