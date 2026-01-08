acd409cf657368a9648d48b3837baf10916672e3
andrewfowlie <andrew.j.fowlie@gmail.com>
2026-01-08 13:32:03 +0800

    add BP2 and BP3

--- src/TransitionSolver/__init__.py
+++ src/TransitionSolver/__init__.py
@@ -4,7 +4,7 @@ TransitionSolver
 """
 
 from .effective_potential import load_potential
-from .benchmarks import RSS_BP1
+from .benchmarks import RSS_BP1, RSS_BP2, RSS_BP3
 from .phasetracer import read_phase_tracer, run_phase_tracer, build_phase_tracer, phase_tracer_info
 from .phasehistory import find_phase_history
 from .plot import plot_summary
