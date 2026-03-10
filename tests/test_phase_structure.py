479bf63885ab3b49487a71ca22b0cb36c7792e8a
Andrew Fowlie <andrew.j.fowlie@gmail.com>
2026-03-10 14:01:46 +0000

    generalise test to be used for any benchmark

--- tests/test_phase_structure.py
+++ tests/test_phase_structure.py
@@ -6,18 +6,22 @@ Test phase structure report
 import os
 from pathlib import Path
 
-from TransitionSolver import phasehistory, RSS_BP1, read_phase_tracer
+import pytest
+
+from TransitionSolver import phasehistory, benchmarks, read_phase_tracer
 from dictcmp import assert_deep_equal
 
 THIS = Path(os.path.dirname(os.path.abspath(__file__)))
 BASELINE = THIS / "baseline"
 
 
-def test_phase_structure(generate_baseline):
-    phase_tracer_file = BASELINE / "rss_bp1_phase_structure.dat"
+@pytest.mark.parametrize("name", ["RSS_BP1"])
+def test_phase_structure(generate_baseline, name):
+    phase_tracer_file = BASELINE / f"{name.lower()}_phase_structure.dat"
     with open(phase_tracer_file) as f:
         phase_tracer_data = f.read()
     phase_structure = read_phase_tracer(phase_tracer_data)
+    model = getattr(benchmarks, name)
     result = phasehistory.find_phase_history(
-        RSS_BP1, phase_structure, bubble_wall_velocity=1)
-    assert_deep_equal(result, BASELINE / "rss_bp1_phase_structure.json", exclude_types=[list], significant_digits=4, generate_baseline=generate_baseline)
+        model, phase_structure, bubble_wall_velocity=1)
+    assert_deep_equal(result, BASELINE / f"{name.lower()}_phase_structure.json", exclude_types=[list], significant_digits=4, generate_baseline=generate_baseline)
