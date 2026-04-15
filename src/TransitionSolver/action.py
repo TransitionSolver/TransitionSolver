c2de68e3afc0dcfcc5080cf1234ad47371503145
Peter Athron <peter.athron@coepp.org.au>
2026-04-15 13:15:03 +0800

    delete passing of extend_to_minima setting -- this was not actually doing anything in PT anyway and has been deleted from there today as well.

--- src/TransitionSolver/action.py
+++ src/TransitionSolver/action.py
@@ -44,13 +44,12 @@ def action_ct(potential, T, false_vacuum, true_vacuum, verbose=False, **kwargs):
         return pathDeformation.fullTunneling(path_points, V_ct, dV_ct,
             verbose=verbose, **kwargs).action
 
-def action_pt(potential, T, false_vacuum, true_vacuum, extend_to_minima=False, **kwargs):
+def action_pt(potential, T, false_vacuum, true_vacuum, **kwargs):
     """
     @returns Action from PhaseTracer's path deformation algorithm
     """
     action_calculator = PhaseTracer.ActionCalculator(potential)
     action_calculator.__python_owns__ = False
-    action_calculator.set_PD_extend_to_minima(extend_to_minima)
 
     if 'maxiter' in kwargs:
         action_calculator.set_PD_path_maxiter(kwargs['maxiter'])
