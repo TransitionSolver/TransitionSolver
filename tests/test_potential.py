39490799e3084c27486894e03aa96ab62c9f432d
Peter Athron <peter.athron@coepp.org.au>
2026-05-22 17:24:21 +0800

    add more BPs to potential test

--- tests/test_potential.py
+++ tests/test_potential.py
@@ -4,12 +4,26 @@ Test model, including access to CosmoTransitions methods
 """
 
 import numpy as np
-from TransitionSolver import RSS_BP2 as model
+import pytest
+
+from TransitionSolver import benchmarks, RSS_BP2 as model
 
 
 def test_cosmo():
     model.getPhases()
     model.findAllTransitions()
 
-def test_potential():
-    assert np.isclose(model(np.array([10., -10.]), 20.), 3869184455.127249)
+
+POTENTIALS = [
+    ("RSS_BP1", 6807486471.394539),
+    ("RSS_BP2", 3869184455.127249),
+    ("RSS_BP3", 27524540814.135723),
+    ("RSS_BP4", 5199730932.906029),
+    ("RSS_BP13", 3526627688.129584),
+]
+
+
+@pytest.mark.parametrize("name, expected", POTENTIALS)
+def test_potential(name, expected):
+    test_model = getattr(benchmarks, name)
+    assert np.isclose(test_model(np.array([10., -10.]), 20.), expected)
